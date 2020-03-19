fd0d1118a164f026759bd3cbab4bfb15fffe3c25
a-samoylov <a.samoylovua@gmail.com>
2020-03-19 21:37:22 +0200

    index controller

---
+++ src/Controller/IndexController.php
@@ -0,0 +1,22 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+class IndexController extends AbstractController
+{
+    /**
+     * @Route("/index", name="index")
+     */
+    public function index()
+    {
+        return $this->json([
+            'message' => 'Welcome to your new controller!',
+            'path' => 'src/Controller/IndexController.php',
+        ]);
+    }
+}
