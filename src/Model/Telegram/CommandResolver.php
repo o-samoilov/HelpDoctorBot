8b6d4f2450e8725269d6e7e187c7b635efe7b5e7
a-samoylov <a.samoylovua@gmail.com>
2020-03-20 17:43:45 +0200

    pa

--- src/Model/Telegram/CommandResolver.php
+++ src/Model/Telegram/CommandResolver.php
@@ -6,11 +6,21 @@ namespace App\Model\Telegram;
 
 class CommandResolver
 {
+    /** @var \App\Repository\UserRepository */
+    private $userRepository;
+
+    public function __construct(\App\Repository\UserRepository $userRepository)
+    {
+        $this->userRepository = $userRepository;
+    }
+
     // ########################################
 
     public function resolve(\TgBotApi\BotApiBase\Type\UpdateType $update): \App\Model\Telegram\Command\BaseAbstract
     {
+        $from = $update->message->from;
 
+        //$this->userRepository->find()
     }
 
     // ########################################
